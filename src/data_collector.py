7c3e7e232fde9d5a75dee36256fbb422ddb437ce
eugepemi <al185399@uji.es>
2023-05-10 20:39:44 +0200

    Added comments, and the ingestor process

--- src/data_collector.py
+++ src/data_collector.py
@@ -1,30 +1,44 @@
 import websocket
 import json
+from storage_ingestion import AzureBlobIngestor
+
+# Instantiating the Azure Blob ingestor
+ingestor = AzureBlobIngestor()
 
 def on_open(ws):
     """
-    
+    Callback executed at socket opening
     """
     print("Connection opened")
-
     subscribe_message = {
         "type": "subscribe",
         "channels": [
             {
                 "name": "ticker",
                 "product_ids": [
-                    "BTC-USD"
+                    # TODO: Hardcoded value, change to a variable
+                    "BTC-USD"       
                 ]
             }
         ]
     }
-
     ws.send(json.dumps(subscribe_message))
 
 def on_message(ws, message):
+    """
+    Function to be executed when a message is received from the WebSocket.
+    """
     print("Message received")
-    print(json.loads(message))
+    data = json.loads(message)
+    print(data)
+    # Ingest the data into Azure Blob Storage
+    ingestor.store_data(data)
 
+# WebSocket URL for Coinbase Pro
 socket = "wss://ws-feed.pro.coinbase.com"
+
+# Instantiate the WebSocketApp and set the callback functions
 ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message)
+
+# Start the WebSocket connection and run forever
 ws.run_forever()
