35e9ee6db1510388326a550ac699c818e7bec804
eugepemi <al185399@uji.es>
2023-05-11 12:16:40 +0200

    Change the code to use async/await to run concurrently tasks for n tickers that we want collect data from

--- src/data_collector.py
+++ src/data_collector.py
@@ -1,44 +1,61 @@
-import websocket
 import json
 from storage_ingestion import AzureBlobStorage
+import asyncio
+import websockets
 
-# Instantiating the Azure Blob ingestor
 ingestor = AzureBlobStorage()
 
-def on_open(ws):
+async def subscribe_ticker(ticker):
     """
-    Callback executed at socket opening
+    This function is an asynchronous function that connects to a WebSocket, subscribes to a specific ticker, 
+    and stores the received messages in Azure Blob Storage.
+
+    Args:
+        ticker (str): The ticker to subscribe to
     """
-    print("Connection opened")
-    subscribe_message = {
-        "type": "subscribe",
-        "channels": [
-            {
-                "name": "ticker",
-                "product_ids": [
-                    # TODO: Hardcoded value, change to a variable
-                    "BTC-USD"       
-                ]
-            }
-        ]
-    }
-    ws.send(json.dumps(subscribe_message))
+    # Connect to the WebSocket
+    async with websockets.connect('wss://ws-feed.pro.coinbase.com') as ws:
+        # Define the subscription message
+        subscribe_message = {
+            "type": "subscribe",
+            "channels": [
+                {
+                    "name": "ticker",
+                    "product_ids": [
+                        ticker
+                    ]
+                }
+            ]
+        }
+        # Send the subscription message
+        await ws.send(json.dumps(subscribe_message))
 
-def on_message(ws, message):
+        # Process received messages
+        async for message in ws:
+            print(f"Message received for {ticker}")
+            data = json.loads(message)
+            print(data)
+            # If the message includes a 'product_id' field, store it in Azure Blob Storage
+            if 'product_id' in data:
+                ingestor.insert_data(data, "bronze")
+
+# Define the list of tickers to subscribe to
+tickers = ["BTC-USD", "ETH-USD"]  # TODO: avoid hardcoding
+
+async def main():
     """
-    Function to be executed when a message is received from the WebSocket.
+    This function creates a list of tasks, each of which is a coroutine for subscribing to a specific ticker.
+    It then runs all these tasks concurrently.
     """
-    print("Message received")
-    data = json.loads(message)
-    print(data)
-    # Ingest the data into Azure Blob Storage
-    ingestor.insert_data(data, 'bronze')
+    # Create a list of tasks
+    tasks = []
+    for ticker in tickers:
+        # Each task is a subscribe_ticker coroutine for each ticker
+        task = subscribe_ticker(ticker)
+        tasks.append(task)
+    
+    # Run all tasks concurrently
+    await asyncio.gather(*tasks)
 
-# WebSocket URL for Coinbase Pro
-socket = "wss://ws-feed.pro.coinbase.com"
-
-# Instantiate the WebSocketApp and set the callback functions
-ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message)
-
-# Start the WebSocket connection and run forever
-ws.run_forever()
+# Run the main function
+asyncio.run(main())
